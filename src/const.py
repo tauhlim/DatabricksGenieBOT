3a6433d0d78a536aac8d4a17bb449c39352e25cf
Tau Herng Lim <tauhlim@gmail.com>
2025-07-09 15:26:17 +0800

    Add required variable to use OAuth

--- src/const.py
+++ src/const.py
@@ -15,6 +15,7 @@ DATABRICKS_CLIENT_ID = os.getenv("DATABRICKS_CLIENT_ID")
 DATABRICKS_CLIENT_SECRET = os.getenv("DATABRICKS_CLIENT_SECRET")
 APP_ID = os.getenv("APP_ID", "")
 APP_PASSWORD = os.getenv("APP_PASSWORD", "")
+OAUTH_CONNECTION_NAME = os.getenv("OAUTH_CONNECTION_NAME", "")
 WELCOME_MESSAGE = "Welcome to the Data Query Bot!"
 WAITING_MESSAGE = "Querying Genie for results..."
 SWITCHING_MESSAGE = "switch to @"
